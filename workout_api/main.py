d5d9cf610a769d5e4a068630951e660ed6a63086
Janei Vieira <taguardado.net@gmail.com>
2025-08-30 17:08:43 -0400

    Update main.py

--- workout_api/main.py
+++ workout_api/main.py
@@ -1,5 +1,9 @@
+# workout_api/main.py (VERSÃO ATUALIZADA)
+
 from fastapi import FastAPI
 from workout_api.routers import api_router
+from fastapi_pagination import add_pagination # Importe
 
 app = FastAPI(title='WorkoutApi')
 app.include_router(api_router)
+add_pagination(app) # Adicione esta linha
